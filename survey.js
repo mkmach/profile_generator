4b91b37f6efb9aaa4a829fdd1d4b8999e1384e77
mkmach <mkmach@hotmail.com>
2022-12-09 10:27:26 -0800

    added new lines after each sentence

--- survey.js
+++ survey.js
@@ -11,7 +11,7 @@ rl.question("What's your name? ", (answer1) => {
       rl.question('Which meal is your favourite ', (answer4) => {
         rl.question('Which sport is your absolute favourite ', (answer5) => {
           rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer6) => {
-            console.log(`Your name is ${answer1}, and your favourite activity to do is ${answer2}, while listening to ${answer3}. Your fave meal is ${answer4}. Your favourite sport is ${answer5}. Your secret super power is to ${answer6}`);
+            console.log(`Your name is ${answer1}, and your favourite activity to do is ${answer2}, while listening to ${answer3}.\n Your fave meal is ${answer4}.\n Your favourite sport is ${answer5}.\n Your secret super power is to ${answer6}`);
 
             rl.close();
           });
